1853668250db3d89bd3fb05e47c75f12ca4073b4
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2022-12-15 23:41:18 +0300

    add Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Navbar from './components/Navbar';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import  Home from "./components/Home";
+import Blog from "./components/Blog";
+import Contact from './components/Contact';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <div><Navbar /></div>
+
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/blog' element={<Blog/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+
+
+
       
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,92 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+     <header aria-label="Site Header" class="bg-white h-36  ">
+  <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 ">
+    <div class="flex h-36 items-center justify-between ">
+      <div class="md:flex md:items-center md:gap-12 ">
+        <a class="block text-gray-600  ml-12 text-xl w-48" href="/">
+          <span class="sr-only">Home</span>
+          <img src='https://neojb.brickthemes.com/wp-content/uploads/2018/09/logo.png' className='' />
+        </a>
+      </div>
+
+      <div class="hidden md:block">
+        <nav aria-label="Site Nav">
+          <ul class="flex items-center gap-14 text-sm font-medium pr-16">
+            <NavLink to="/">
+            <li>
+              <a
+                class="text-black transition hover:text-gray-500/75"
+              >
+                Home
+              </a>
+            </li>
+            </NavLink>
+
+
+           <NavLink to={""}>
+            <li>
+              <a
+                class="text-black transition hover:text-gray-500/75"
+              >
+                Browse Jobs
+              </a>
+            </li>
+            </NavLink>
+
+            <NavLink to={""}>
+            <li>
+              <a
+                class="text-black transition hover:text-gray-500/75"
+              >
+                Post a job
+              </a>
+            </li>
+            </NavLink>
+
+            <NavLink to={"blog"}>
+            <li>
+              <a
+                class="text-black transition hover:text-gray-500/75"
+              >
+                Blog
+              </a>
+            </li>
+            </NavLink>
+
+            <NavLink to={""}>
+            <li>
+              <a
+                class="text-black transition hover:text-gray-500/75"
+              >
+                Account
+              </a>
+            </li>
+            </NavLink>
+
+            <NavLink to={"contact"}>
+            <li>
+              <a
+                class="text-black transition hover:text-gray-500/75"
+              >
+                Contacts
+              </a>
+            </li>
+            </NavLink>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+</header>
+
+
+    </div>
   )
 }
 
