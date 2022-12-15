3f73f5565fa776469d6a81c4935982d389d4a04b
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2022-12-16 00:02:26 +0300

    Add Footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import About from './About'
 import Footer from './Footer'
 
 function Home() {
   return (
     <div>Home
+      <About />
       <Footer />
     </div>
     
