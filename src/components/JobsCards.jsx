29f67b60b9925540fc0de31659f1c619c7e06a6b
+ <+>
2023-01-13 22:15:03 +0300

    Add Job

--- src/components/JobsCards.jsx
+++ src/components/JobsCards.jsx
@@ -12,11 +12,59 @@ const JobsCards = (props) => {
             location="Nairobi, Kenya"
             employmentType="Full-time"
           />
+          <FeaturedJobsCard
+            name="Featured Jobs"
+            title="DevOps Engineer"
+            location="Nairobi, Kenya"
+            employmentType="Full-time"
+          />
+          <FeaturedJobsCard
+            name="Featured Jobs"
+            title="Front-end Developer"
+            location="Nairobi, Kenya"
+            employmentType="Full-time"
+          />
+          <FeaturedJobsCard
+            name="Featured Jobs"
+            title="Back-end Developer"
+            location="Nairobi, Kenya"
+            employmentType="Full-time"
+          />
+          <FeaturedJobsCard
+            name="Featured Jobs"
+            title="QA Engineer"
+            location="Nairobi, Kenya"
+            employmentType="Full-time"
+          />
         </div>
         <div style={{ border: '2px solid #ccc', padding: '32px', margin: '16px' }}>
           <RecentJobsCard
             name="Recent Jobs"
             title="Data Scientist"
+            location="Nakuru, Kenya"
+            employmentType="Part-time"
+          />
+          <RecentJobsCard
+            name="Recent Jobs"
+            title="Data Analyst"
+            location="Nakuru, Kenya"
+            employmentType="Part-time"
+          />
+          <RecentJobsCard
+            name="Recent Jobs"
+            title="Business Analyst"
+            location="Nakuru, Kenya"
+            employmentType="Part-time"
+          />
+          <RecentJobsCard
+            name="Recent Jobs"
+            title="Product Manager"
+            location="Nakuru, Kenya"
+            employmentType="Part-time"
+          />
+          <RecentJobsCard
+            name="Recent Jobs"
+            title="Project Manager"
             location="Nakuru, Kenya"
             employmentType="Part-time"
           />
@@ -26,4 +74,3 @@ const JobsCards = (props) => {
   }
   
   export default JobsCards;
-  
