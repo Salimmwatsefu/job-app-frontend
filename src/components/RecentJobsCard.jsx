c59e72d1bad370acaa29bb32dba3eefbf3675d71
+ <+>
2023-01-16 15:38:38 +0300

    Add jobcard

--- src/components/RecentJobsCard.jsx
+++ src/components/RecentJobsCard.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 const RecentJobsCard = (props) => {
   return (
     <div>
-      <br/>
-      <h2 >Recent Jobs</h2>
-      <br/>
+      <h2 style={{ position: 'absolute', top: '0', Right: '0' }}>{props.name}</h2>
       <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <div style={{ marginRight: '16px' }}></div>
-        <div style={{ marginRight: '16px' }}></div>
-        <div></div>
+        <div style={{ marginRight: '16px' }}>{props.title}</div>
+        <div style={{ marginRight: '16px' }}>{props.location}</div>
+        <div>{props.employmentType}</div>
       </div>
     </div>
   );
